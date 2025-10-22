be13c374b63b736903414be749fac813a0bf2891
harmansandhu-glitch <harmanjotsingh200310@gmail.com>
2025-10-22 08:26:18 +0530

    fix: Add afterSignOutUrl to ClerkProvider for Vercel deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider 
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/"
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
